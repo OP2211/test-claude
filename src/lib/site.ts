dcf88da1cd7c69aeaaac8549ab715a37a5ad0507
Tushar Vaghela <tushar.v@proximity.tech>
2026-04-14 14:40:55 +0530

    feat:social (#8)

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,6 @@
-/** Replace with your real profile URLs when ready. */
 export const SOCIAL_LINKS = [
   { id: 'x', label: 'FanGround on X', href: 'https://x.com' },
-  { id: 'instagram', label: 'FanGround on Instagram', href: 'https://www.instagram.com' },
+  { id: 'instagram', label: 'FanGround on Instagram', href: 'https://www.instagram.com/fanground.online' },
   { id: 'facebook', label: 'FanGround on Facebook', href: 'https://www.facebook.com' },
   { id: 'youtube', label: 'FanGround on YouTube', href: 'https://www.youtube.com' },
 ] as const;
